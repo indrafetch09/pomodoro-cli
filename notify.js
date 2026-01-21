889765f6ebb9837cd8a5ebd2211f6b99b1244670
Kaiden <harindrawahyu131@gmail.com>
2026-01-21 23:49:24 +0700

    refactor Promise to notify function

--- notify.js
+++ notify.js
@@ -3,7 +3,9 @@ import { execFile } from "child_process";
 
 const notifier = pkg;
 
+//TODO: refactor function notify
 export function notify(title = "Pomodoro CLI", message = "", cb) {
+<<<<<<< Updated upstream
     try {
         if (notifier && typeof notifier.notify === "function") {
             notifier.notify({ title, message, sound: true }, (err) => {
@@ -14,6 +16,23 @@ export function notify(title = "Pomodoro CLI", message = "", cb) {
                 }
             });
             return;
+=======
+    const p = new Promise((resolve, reject) => {
+        // twice try catch block 
+        try {
+            if (notifier && typeof notifier.notify === "function") {
+                notifier.notify({ title, message, sound: true }, (err) => {
+                    if (err) {
+                        fallBackNotify(title, message)
+                            .then(() => playSound().then(resolve).catch(resolve))
+                    } else if (typeof cb === "function") {
+                        cb(null);
+                    }
+                });
+                return;
+            }
+        } catch (e) {
+>>>>>>> Stashed changes
         }
     } catch (e) {
     }
@@ -21,7 +40,13 @@ export function notify(title = "Pomodoro CLI", message = "", cb) {
     tryNotifySend(title, message, cb);
 }
 
+function promisifyNotify() {
+    return new Promise
+    // return new Promise from notifier.notify and set params resolve, reject
+}
+
 function tryNotifySend(title, message, cb) {
+    // return new Promise set params resolve, reject
     execFile("notify-send", [ title, message ], (err) => {
         if (err) {
             console.log(`\x07${title}: ${message}`);
